ca1a3dafa58c1e77cefca0c955b02a9178f48495
Ennin Francis <enninfrancis47@gmail.com>
2021-05-10 21:37:37 +0000

    make it better

--- nsromahub_project_api/settings.py
+++ nsromahub_project_api/settings.py
@@ -22,9 +22,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["nsrohub.herokuapp.com","localhost","127.0.0.1"]
 
 
 # Application definition
